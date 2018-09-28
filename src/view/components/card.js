374f302772263f322836cb4095b4754f54b9105c
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-09-28 15:10:47 +0200

    fix car component

--- src/view/components/card.js
+++ src/view/components/card.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 
-class Card extends Component {
+class Card extends PureComponent {
   render() {
     return <img src={require(`../../assets/${this.props.selected}.svg`)} />;
   }
