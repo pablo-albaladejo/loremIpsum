0cb2e5e3fe4d63c12785e0adf6f5636017e855ab
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-09-28 10:44:35 +0200

    Structure added

---
+++ lorem-ipsum/src/view/screens/game.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Game extends PureComponent {
+  state = {
+    turn: '',
+    players: [],
+    options: ['Scissors', 'Spock', 'Paper', 'Lizard', 'Rock']
+  };
+
+  getResult = ({ option }) => {
+    return {};
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Here's the Game</h1>
+      </div>
+    );
+  }
+}
+
+Game.propTypes = {};
+
+Game.defaultProps = {};
+
+export default Game;
