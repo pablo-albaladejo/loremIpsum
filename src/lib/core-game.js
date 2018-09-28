c530a499694a7f1825f293b5634bcadcb94e7e21
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2018-09-28 13:02:32 +0200

    chore: constants file added

--- src/lib/core-game.js
+++ src/lib/core-game.js
@@ -1,5 +1,53 @@
 import { submitPlay } from './api-service';
 
+import CONSTANTS from './constants';
+
+function play(playerOne, playerTwo) {
+  const table = [
+    CONSTANTS.CARDS.SCISSORS = [
+      CONSTANTS.CARDS.SCISSORS = CONSTANTS.GAMBLE.TIE,
+      CONSTANTS.CARDS.PAPER = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.ROCK = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.LIZARD = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.SPOCK = CONSTANTS.GAMBLE.LOSE,
+    ],
+
+    CONSTANTS.CARDS.PAPER = [
+      CONSTANTS.CARDS.SCISSORS = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.PAPER = CONSTANTS.GAMBLE.TIE,
+      CONSTANTS.CARDS.ROCK = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.LIZARD = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.SPOCK = CONSTANTS.GAMBLE.WIN,
+    ],
+
+    CONSTANTS.CARDS.ROCK = [
+      CONSTANTS.CARDS.SCISSORS = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.PAPER = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.ROCK = CONSTANTS.GAMBLE.TIE,
+      CONSTANTS.CARDS.LIZARD = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.SPOCK = CONSTANTS.GAMBLE.LOSE,
+    ],
+
+    CONSTANTS.CARDS.LIZARD = [
+      CONSTANTS.CARDS.SCISSORS = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.PAPER = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.ROCK = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.LIZARD = CONSTANTS.GAMBLE.TIE,
+      CONSTANTS.CARDS.SPOCK = CONSTANTS.GAMBLE.WIN,
+    ],
+
+    CONSTANTS.CARDS.SPOCK = [
+      CONSTANTS.CARDS.SCISSORS = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.PAPER = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.ROCK = CONSTANTS.GAMBLE.WIN,
+      CONSTANTS.CARDS.LIZARD = CONSTANTS.GAMBLE.LOSE,
+      CONSTANTS.CARDS.SPOCK = CONSTANTS.GAMBLE.TIE,
+    ]
+  ];
+
+  return table[playerOne][playerTwo];
+}
+
 function getComputerOption(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -12,14 +60,41 @@ function canUserPlay(bet, balance) {
   return bet < balance;
 }
 
-function game() {
-  const result = 0;
+function updateBalance(balance, bet, gamble) {
 
+  switch (gamble) {
+
+    case CONSTANTS.GAMBLE.LOSE:
+      return balance - bet;
+      break;
+
+    case CONSTANTS.GAMBLE.WIN:
+      return balance + bet;
+      break;
+
+    default:
+      return balance
+      break;
+
+  }
+
+}
+
+function game(playerOne, bet) {
+  const playerTwo = getComputerOption(Object.keys(CONSTANTS.CARDS).length);
+
+  const result = 0;
   const balance = 100;
 
+  const gamble = play(playerOne, playerTwo);
+
   return {
-    result,
-    balance
+    result: {
+      status: CONSTANTS.STATUS.SUCCESS,
+      gamble,
+      playerTwo,
+    },
+    balance: updateBalance(balance, bet, gamble)
   };
 }
 
